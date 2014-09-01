8f6895e370de7d93a148a29197b0b9c82bc8d0b1
ircary <ircary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-01 20:58:47 +0000

    = 1.4.4 = 	- Fixed login shortcode
    git-svn-id: https://plugins.svn.wordpress.org/lct-useful-shortcodes-functions/trunk@976696 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- misc/login.php
+++ misc/login.php
@@ -3,25 +3,25 @@ add_action( 'lct_login_css', 'lct_login_css' );
 function lct_login_css() {
 	$g_lusf = new g_lusf;
 
-    wp_register_style( 'lct_login_css', $g_lusf->plugin_dir_url . 'assets/css/login.css' );
-    wp_enqueue_style( 'lct_login_css' );
+	wp_register_style( 'lct_login_css', $g_lusf->plugin_dir_url . 'assets/css/login.css' );
+	wp_enqueue_style( 'lct_login_css' );
 }
 
 
-function getLostLink($c){
+function getLostLink( $c ) {
 	$params = array( 'action' => "lostpassword" );
 	$url = add_query_arg( $params, get_permalink() );
 	return $url;
 }
-add_filter('lostpassword_url','getLostLink');
+add_filter( 'lostpassword_url', 'getLostLink' );
 
 
-function logout_redirect($c) {
+function logout_redirect( $c ) {
 	$params = array( 'redirect_to' => get_permalink() );
-	$url = add_query_arg( $params ,$c);
-    return $url;
+	$url = add_query_arg( $params, $c );
+	return $url;
 }
-add_filter('logout_url','logout_redirect');
+add_filter( 'logout_url', 'logout_redirect' );
 
 
 function retrieve_my_password() {
@@ -29,24 +29,25 @@ function retrieve_my_password() {
 
 	$errors = new WP_Error();
 
-	if ( empty( $_POST['user_login'] ) ) {
-		$errors->add('empty_username', __('<strong>ERROR</strong>: Enter a username or e-mail address.'));
-	} else if ( strpos( $_POST['user_login'], '@' ) ) {
-		$user_data = get_user_by( 'email', trim( $_POST['user_login'] ) );
-		if ( empty( $user_data ) )
-			$errors->add('invalid_email', __('<strong>ERROR</strong>: There is no user registered with that email address.'));
-	} else {
-		$login = trim($_POST['user_login']);
-		$user_data = get_user_by('login', $login);
-	}
+	if( empty( $_POST['user_login'] ) ) {
+		$errors->add( 'empty_username', __( '<strong>ERROR</strong>: Enter a username or e-mail address.' ) );
+	} else
+		if( strpos( $_POST['user_login'], '@' ) ) {
+			$user_data = get_user_by( 'email', trim( $_POST['user_login'] ) );
+			if( empty( $user_data ) )
+				$errors->add( 'invalid_email', __( '<strong>ERROR</strong>: There is no user registered with that email address.' ) );
+		} else {
+			$login = trim( $_POST['user_login'] );
+			$user_data = get_user_by( 'login', $login );
+		}
 
-	do_action('lostpassword_post');
+		do_action( 'lostpassword_post' );
 
-	if ( $errors->get_error_code() )
+	if( $errors->get_error_code() )
 		return $errors;
 
-	if ( !$user_data ) {
-		$errors->add('invalidcombo', __('<strong>ERROR</strong>: Invalid username or e-mail.'));
+	if( ! $user_data ) {
+		$errors->add( 'invalidcombo', __( '<strong>ERROR</strong>: Invalid username or e-mail.' ) );
 		return $errors;
 	}
 
@@ -54,56 +55,58 @@ function retrieve_my_password() {
 	$user_login = $user_data->user_login;
 	$user_email = $user_data->user_email;
 
-	do_action('retreive_password', $user_login);  // Misspelled and deprecated
-	do_action('retrieve_password', $user_login);
+	do_action( 'retreive_password', $user_login ); // Misspelled and deprecated
+	do_action( 'retrieve_password', $user_login );
 
-	$allow = apply_filters('allow_password_reset', true, $user_data->ID);
+	$allow = apply_filters( 'allow_password_reset', true, $user_data->ID );
 
-	if ( ! $allow )
-		return new WP_Error('no_password_reset', __('Password reset is not allowed for this user'));
-	else if ( is_wp_error($allow) )
-		return $allow;
-
-	$key = $wpdb->get_var($wpdb->prepare("SELECT user_activation_key FROM $wpdb->users WHERE user_login = %s", $user_login));
-	if ( empty($key) ) {
-		// Generate something random for a key...
-		$key = wp_generate_password(20, false);
-		do_action('retrieve_password_key', $user_login, $key);
-		// Now insert the new md5 key into the db
-		$wpdb->update($wpdb->users, array('user_activation_key' => $key), array('user_login' => $user_login));
-	}
-	$message = __('Someone requested that the password be reset for the following account:') . "\r\n\r\n";
-	$message .= network_site_url() . "\r\n\r\n";
-	$message .= sprintf(__('Username: %s'), $user_login) . "\r\n\r\n";
-	$message .= __('If this was a mistake, just ignore this email and nothing will happen.') . "\r\n\r\n";
-	$message .= __('To reset your password, visit the following address:') . "\r\n\r\n";
-	$message .= '<' . network_site_url("wp-login.php?action=rp&key=$key&login=" . rawurlencode($user_login), 'login') . ">\r\n";
-
-	if ( is_multisite() )
-		$blogname = $GLOBALS['current_site']->site_name;
+	if( ! $allow )
+		return new WP_Error( 'no_password_reset', __( 'Password reset is not allowed for this user' ) );
 	else
-		// The blogname option is escaped with esc_html on the way into the database in sanitize_option
+		if( is_wp_error( $allow ) )
+			return $allow;
+
+	$key = $wpdb->get_var( $wpdb->prepare( "SELECT user_activation_key FROM $wpdb->users WHERE user_login = %s", $user_login ) );
+	if( empty( $key ) ) {
+		// Generate something random for a key...
+		$key = wp_generate_password( 20, false );
+		do_action( 'retrieve_password_key', $user_login, $key );
+		// Now insert the new md5 key into the db
+		$wpdb->update( $wpdb->users, array( 'user_activation_key' => $key ), array( 'user_login' => $user_login ) );
+	}
+	$message = __( 'Someone requested that the password be reset for the following account:' ) . "\r\n\r\n";
+	$message .= network_site_url() . "\r\n\r\n";
+	$message .= sprintf( __( 'Username: %s' ), $user_login ) . "\r\n\r\n";
+	$message .= __( 'If this was a mistake, just ignore this email and nothing will happen.' ) . "\r\n\r\n";
+	$message .= __( 'To reset your password, visit the following address:' ) . "\r\n\r\n";
+	$message .= '<' . network_site_url( "wp-login.php?action=rp&key=$key&login=" . rawurlencode( $user_login ), 'login' ) . ">\r\n";
+
+	if( is_multisite() )
+		$blogname = $GLOBALS['current_site']->site_name;
+	else // The blogname option is escaped with esc_html on the way into the database in sanitize_option
 		// we want to reverse this for the plain text arena of emails.
-		$blogname = wp_specialchars_decode(get_option('blogname'), ENT_QUOTES);
 
-	$title = sprintf( __('[%s] Password Reset'), $blogname );
+		$blogname = wp_specialchars_decode( get_option( 'blogname' ), ENT_QUOTES );
 
-	$title = apply_filters('retrieve_password_title', $title);
-	$message = apply_filters('retrieve_password_message', $message, $key);
+	$title = sprintf( __( '[%s] Password Reset' ), $blogname );
 
-	if ( $message && !wp_mail($user_email, $title, $message) ){
+	$title = apply_filters( 'retrieve_password_title', $title );
+	$message = apply_filters( 'retrieve_password_message', $message, $key );
 
-		$errors->add('invalidcombo', __('<strong>ERROR</strong>: The e-mail could not be sent. <br /> Possible reason: your host may have disabled the mail() function...'));
+	if( $message && ! wp_mail( $user_email, $title, $message ) ) {
+
+		$errors->add( 'invalidcombo', __( '<strong>ERROR</strong>: The e-mail could not be sent. <br /> Possible reason: your host may have disabled the mail() function...' ) );
 		return $errors;
-		}
+	}
 	return true;
 }
 
 
-function displayForm($atts){
-	do_action( 'lct_login_css' ); ?>
-	<?php if(isset($_POST['submit'])){
-		if(isset($_POST['username']) && isset($_POST['password'])){
+function displayForm( $atts ) {
+	do_action( 'lct_login_css' );
+
+	if( isset( $_POST['submit'] ) ) {
+		if( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
 			$creds = array();
 			$creds['user_login'] = $_POST['username'];
 			$creds['user_password'] = $_POST['password'];
@@ -112,108 +115,109 @@ function displayForm($atts){
 			$user = wp_signon( $creds, false );
 			if ( is_wp_error($user) )
 			   $jerror = '<div class="jerror">'.$user->get_error_message().'</div>';
-		}
-		else{
+		}else{
 			$jerror = '<div class="jerror">Enter Username and password.</div>';
 		}
 	}
+
 	//Password retrive section
-	if(isset($_POST['user_login'])){
-	$result = retrieve_my_password();
-		if ( is_wp_error($result) )
+	if( isset( $_POST['user_login'] ) ) {
+		$result = retrieve_my_password();
+		if( is_wp_error($result) )
 			$jerror = '<div class="jerror">'.$result->get_error_message().'</div>';
 	}
 
-	if(isset($_GET['action']) && !is_user_logged_in()){
+
+	if( isset( $_GET['action'] ) && ! is_user_logged_in() ) { ?>
+		<section id="contentForm">
+			<!--form name="lostpasswordform" id="lostpasswordform" action="<?php echo home_url('/'); ?>/wp-login.php?action=lostpassword" method="post"-->
+			<form name="lostpasswordform" id="lostpasswordform" action="" method="post">
+				<h1>Reset</h1>
+				<div>
+					<input type="text" name="user_login" id="user_login" placeholder="Username or E-mail" value="" required="" size="20" tabindex="10"></label>
+				</div>
+				<div>
+					<input type="hidden" name="redirect_to" value="<?php echo get_permalink(); ?>">
+					<input type="submit" name="wp-submit" id="wp-submit" class="button-primary" value="Get Password" tabindex="100">
+					<a href="<?php echo get_permalink();  ?>">Login Now</a>
+				</div>
+			</form>
+		</section><!-- content -->
 
 
-	?>
-	<section id="contentForm">
-		<!--form name="lostpasswordform" id="lostpasswordform" action="<?php echo home_url('/'); ?>/wp-login.php?action=lostpassword" method="post"-->
-		<form name="lostpasswordform" id="lostpasswordform" action="" method="post">
-			<h1>Reset</h1>
-			<div>
-				<input type="text" name="user_login" id="user_login" placeholder="Username or E-mail" value="" required="" size="20" tabindex="10"></label>
-			</div>
-			<div>
-				<input type="hidden" name="redirect_to" value="<?php echo get_permalink(); ?>">
-				<input type="submit" name="wp-submit" id="wp-submit" class="button-primary" value="Get Password" tabindex="100">
-				<a href="<?php echo get_permalink();  ?>">Login Now</a>
-			</div>
-		</form>
-	</section><!-- content -->
-	<?
-	}
-	else if(!is_user_logged_in()){
-    ?>
+	<?php } else if( ! is_user_logged_in() ) {
 
-    <?php if( function_exists( 'presscore_get_logo_image' ) && lct_get_lct_useful_settings( 'lct_show_login_logo' ) ) {
-		if( $bottom_logo = presscore_get_logo_image( presscore_get_footer_logos_meta() ) ) {
-	    	echo "<p class='login_logo'>$bottom_logo</p>";
-	    }
-	}
 
-	if( ! $bottom_logo && lct_get_lct_useful_settings( 'lct_show_login_logo' ) && lct_get_lct_useful_settings( 'lct_login_logo' ) ) {
-    	echo "<p class='login_logo'><img src='" . lct_get_lct_useful_settings( 'lct_login_logo' ) . "' /></p>";
-    } ?>
+	    if( function_exists( 'presscore_get_logo_image' ) && lct_get_lct_useful_settings( 'lct_show_login_logo' ) ) {
+			if( $bottom_logo = presscore_get_logo_image( presscore_get_footer_logos_meta() ) ) {
+		    	echo "<p class='login_logo'>$bottom_logo</p>";
+		    }
+		}
 
-    <?php echo $jerror; ?>
-	<section id="contentForm">
-		<form name="loginform" id="loginform" action="" method="post">
+		if( ! $bottom_logo && lct_get_lct_useful_settings( 'lct_show_login_logo' ) && lct_get_lct_useful_settings( 'lct_login_logo' ) ) {
+	    	echo "<p class='login_logo'><img src='" . lct_get_lct_useful_settings( 'lct_login_logo' ) . "' /></p>";
+	    } ?>
 
-			<?php if( lct_get_lct_useful_settings( 'lct_tag_line' ) && lct_get_lct_useful_settings( 'lct_show_tag_line' ) ) {
-		    	echo "<h1>" . lct_get_lct_useful_settings( 'lct_tag_line' ) . "</h1>";
-		    }elseif( get_bloginfo( 'description' ) && lct_get_lct_useful_settings( 'lct_show_tag_line' ) ) {
-		    	echo "<h1>" . get_bloginfo( 'description' ) . "</h1>";
-		    }else{
-		    	echo "<h1></h1>";
-		    } ?>
+	    <?php echo $jerror; ?>
+		<section id="contentForm">
+			<form name="loginform" id="loginform" action="" method="post">
 
-			<div>
-				<input type="text" placeholder="USER" value="" id="username" name="username" />
-			</div>
-			<div>
-				<input type="password" placeholder="PASSWORD" value="" id="password" name="password" />
-			</div>
-			<div>
-				<input type="submit" value="Login" name="submit" id="submit" style="display: none;" />
-				<p>
-					<a href="<?php echo wp_lostpassword_url(); ?>">Forgot Password?</a>
+				<?php if( lct_get_lct_useful_settings( 'lct_tag_line' ) && lct_get_lct_useful_settings( 'lct_show_tag_line' ) ) {
+			    	echo "<h1>" . lct_get_lct_useful_settings( 'lct_tag_line' ) . "</h1>";
+			    }elseif( get_bloginfo( 'description' ) && lct_get_lct_useful_settings( 'lct_show_tag_line' ) ) {
+			    	echo "<h1>" . get_bloginfo( 'description' ) . "</h1>";
+			    }else{
+			    	echo "<h1></h1>";
+			    } ?>
 
-					<?php if( lct_get_lct_useful_settings( 'lct_show_register_link' ) ) { ?>
-						&nbsp;<span class="hide-a-pipe">|</span> <a href="/<?php echo the_slug( lct_get_lct_useful_settings( 'lct_register_page' ) ); ?>">New to <?php echo get_bloginfo('name'); ?>?</a>
-					<?php } ?>
-				</p>
-			</div>
-			<?php if( function_exists( 'of_get_option' ) ) {
-				if( $credits = of_get_option('bottom_bar-copyrights', false) ) { ?>
-					<div style="margin-top: 30px;">
-						<p><?php echo $credits ?></p>
-					</div>
-				<?php }
-			} ?>
-		</form><!-- form -->
+				<div>
+					<input type="text" placeholder="USER" value="" id="username" name="username" />
+				</div>
+				<div>
+					<input type="password" placeholder="PASSWORD" value="" id="password" name="password" />
+				</div>
+				<div>
+					<input type="submit" value="Login" name="submit" id="submit" style="display: none;" />
+					<p>
+						<a href="<?php echo wp_lostpassword_url(); ?>">Forgot Password?</a>
 
-	</section><!-- content -->
+						<?php if( lct_get_lct_useful_settings( 'lct_show_register_link' ) ) { ?>
+							&nbsp;<span class="hide-a-pipe">|</span> <a href="/<?php echo the_slug( lct_get_lct_useful_settings( 'lct_register_page' ) ); ?>">New to <?php echo get_bloginfo('name'); ?>?</a>
+						<?php } ?>
+					</p>
+				</div>
+				<?php if( function_exists( 'of_get_option' ) ) {
+					if( $credits = of_get_option('bottom_bar-copyrights', false) ) { ?>
+						<div style="margin-top: 30px;">
+							<p><?php echo $credits ?></p>
+						</div>
+					<?php }
+				} ?>
+			</form><!-- form -->
 
-	<script>
-	var $login = jQuery.noConflict();
-	$login(window).load(function(){
-		if($login('#username').val() != 'USER' && $login('#username').val() != '')
-			$login('#submit').show();
-		else
-			$login('#submit').hide();
+		</section><!-- content -->
 
-		$login('#username').keyup(function(){
+		<script>
+		var $login = jQuery.noConflict();
+		$login(window).load(function(){
 			if($login('#username').val() != 'USER' && $login('#username').val() != '')
 				$login('#submit').show();
 			else
 				$login('#submit').hide();
+
+			$login('#username').keyup(function(){
+				if($login('#username').val() != 'USER' && $login('#username').val() != '')
+					$login('#submit').show();
+				else
+					$login('#submit').hide();
+			});
 		});
-	});
-	</script>
+		</script>
+
 
 	<?php } else if( is_user_logged_in() ) {
+
+
 		$current_user=wp_get_current_user();
 
 		echo '<section id="contentForm">';
@@ -226,7 +230,8 @@ function displayForm($atts){
 	    echo '<div>User display name: ' . $current_user->display_name . '</div><br />';
 		echo '<div><a href="'.wp_logout_url().'" title="Logout">Logout</a><br /></div></form>';
 		echo '</section>';
+
+
 	}
 }
-
 add_shortcode('login_form', 'displayForm');
