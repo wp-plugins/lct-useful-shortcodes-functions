28c19305028519c86c88e57c766e89048a4392a1
ircary <ircary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-19 00:13:30 +0000

    = 1.4.11 = 	- Minor Tweaks
    git-svn-id: https://plugins.svn.wordpress.org/lct-useful-shortcodes-functions/trunk@992955 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/functions.php
+++ admin/functions.php
@@ -47,6 +47,10 @@ add_filter( 'script_loader_src', '_remove_script_version', 15, 1 );
 add_filter( 'style_loader_src', '_remove_script_version', 15, 1 );
 function _remove_script_version( $src ){
 	$parts = explode( '?', $src );
+
+	if( strpos( $src, 'fonts.googleapis.com/css?' ) !== false )
+		return $src;
+
 	return $parts[0];
 }
 
--- readme.txt
+++ readme.txt
@@ -1,7 +1,7 @@
 === LCT Useful Shortcodes & Functions ===
 Contributors: ircary
 Donate link: http://lookclassy.com/
-Stable tag: 1.4.10
+Stable tag: 1.4.11
 Requires at least: 3.3
 Tested up to: 4.0
 Tags: Functions, Shortcodes
@@ -54,6 +54,9 @@ none
 
 
 == Changelog ==
+= 1.4.11 =
+	- Minor Tweaks
+
 = 1.4.10 =
 	- Minor Tweaks
 
