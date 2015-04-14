2566eb65815ba2a569be8b4683e786c212685d6e
ircary <ircary@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-14 14:16:23 +0000

    = 4.1.15 - 4.1.21 = 	- Added Avada Theme Support 	- gform css tweaks 	- Code Cleanup
    git-svn-id: https://plugins.svn.wordpress.org/lct-useful-shortcodes-functions/trunk@1134532 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- lct-useful-shortcodes-functions.php
+++ lct-useful-shortcodes-functions.php
@@ -1,7 +1,7 @@
 <?php /*
 Plugin Name: LCT Useful Shortcodes & Functions
 Plugin URI: http://lookclassy.com/wordpress-plugins/useful-shortcodes-functions/
-Version: 4.1.20
+Version: 4.1.21
 Text Domain: lct-useful-shortcodes-functions
 Author: Look Classy Technologies
 Author URI: http://lookclassy.com/
